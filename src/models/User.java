1f3ab95291af91c044ba23799fc88fd5e97ba812
Tuchanski <gui.rocha2004@gmail.com>
2024-10-29 17:41:51 -0300

    :sparkles: feat: Add User entity, custom User exception, temporary Zoo class, unfinished ZooSystem class

---
+++ src/models/User.java
@@ -0,0 +1,79 @@
+package models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class User {
+
+    private static Long idCounter = 0L;
+
+    private Long id;
+
+    private String username;
+    private String password;
+    
+    private List<Zoo> zoos = new ArrayList<>();
+
+    public User(String username, String password){
+        idCounter++;
+        this.id = idCounter;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Zoo> getZoos() {
+        return zoos;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", username=" + username + "]";
+    }
+
+
+}
---
+++ src/models/Zoo.java
@@ -0,0 +1,7 @@
+package models;
+
+public class Zoo {
+
+    // To be implemented
+
+}

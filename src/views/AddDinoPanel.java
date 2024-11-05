4ae73c8909bb520de90d242945f1546d0e8a83db
luizmatoso <luizhmatoso1340@gmail.com>
2024-11-05 14:39:50 -0300

    :arrow_up: update: Labels and input boxes

--- src/views/AddDinoPanel.java
+++ src/views/AddDinoPanel.java
@@ -21,7 +21,7 @@ public class AddDinoPanel extends JFrame {
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        ImageBackgroundPanel backgroundPanel = new ImageBackgroundPanel("src/resources/images/small-bg.png");
+        ImageBackgroundPanel backgroundPanel = new ImageBackgroundPanel("src/resources/images/small-bg-add.png");
         backgroundPanel.setLayout(null);
 
         backgroundPanel.addMouseListener(new MouseAdapter() {
@@ -40,21 +40,21 @@ public class AddDinoPanel extends JFrame {
         });
 
         CustomButton closeButton = new CustomButton(
-                "src/resources/buttons/closeButton.png",
-                319,
+                "src/resources/buttons/closeButtonSmall.png",
+                335,
                 14,
-                68,
-                70,
+                52,
+                53,
                 e -> System.exit(0),
                 Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
         );
 
         CustomButton minimizeButton = new CustomButton(
-                "src/resources/buttons/minimizeButton.png",
-                250,
+                "src/resources/buttons/minimizeButtonSmall.png",
+                280,
                 14,
-                68,
-                70,
+                52,
+                53,
                 e -> parentFrame.setState(JFrame.ICONIFIED),
                 Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
         );
@@ -62,62 +62,77 @@ public class AddDinoPanel extends JFrame {
         backgroundPanel.add(closeButton);
         backgroundPanel.add(minimizeButton);
 
+        // JLayeredPane - organize layers
+        JLayeredPane layeredPane = new JLayeredPane();
+        layeredPane.setBounds(0, 0, 400, 500);
+        backgroundPanel.add(layeredPane);
 
         // Name
-        JLabel nameLabel = new JLabel("Name:");
-        nameLabel.setForeground(Color.WHITE);
-        nameLabel.setBounds(30, 70, 100, 30);
-        backgroundPanel.add(nameLabel);
+        configFieldWithLabel(layeredPane, "NAME:", 175, 110, 100, 30, 16);
 
-        nameField = new JTextField();
-        nameField.setBackground(new Color(255, 255, 255));
-        nameField.setForeground(new Color(0, 0, 0));
-        nameField.setBorder(null);
-        nameField.setBounds(30, 100, 340, 30);
-        backgroundPanel.add(nameField);
+        ImageIcon nameFieldBg = new ImageIcon("src/resources/images/teste.png");
+        JLabel nameFieldBackground = new JLabel(nameFieldBg);
+        nameFieldBackground.setBounds(65, 140, 266, 47);
+        layeredPane.add(nameFieldBackground, JLayeredPane.DEFAULT_LAYER);
 
+        nameField = transparentField(68, 140, 262, 47, 12);
+        layeredPane.add(nameField, JLayeredPane.PALETTE_LAYER);
 
         // Type
-        JLabel typeLabel = new JLabel("Type:");
-        typeLabel.setForeground(Color.WHITE);
-        typeLabel.setBounds(30, 140, 100, 30);
-        backgroundPanel.add(typeLabel);
+        configFieldWithLabel(layeredPane, "TYPE:", 175, 190, 100, 30, 16);
 
-        typeField = new JTextField();
-        typeField.setBackground(new Color(255, 255, 255));
-        typeField.setForeground(new Color(0, 0, 0));
-        typeField.setBorder(null);
-        typeField.setBounds(30, 170, 340, 30);
-        backgroundPanel.add(typeField);
+        ImageIcon typeFieldBg = new ImageIcon("src/resources/images/teste.png");
+        JLabel typeFieldBackground = new JLabel(typeFieldBg);
+        typeFieldBackground.setBounds(65, 220, 266, 47);
+        layeredPane.add(typeFieldBackground, JLayeredPane.DEFAULT_LAYER);
+
+        typeField = transparentField(68, 220, 262, 47, 12);
+        layeredPane.add(typeField, JLayeredPane.PALETTE_LAYER);
 
         // Age
+        configFieldWithLabel(layeredPane, "AGE:", 178, 270, 100, 30, 16);
 
-        JLabel ageLabel = new JLabel("Age:");
-        ageLabel.setForeground(Color.WHITE);
-        ageLabel.setBounds(30, 200, 100, 30);
-        backgroundPanel.add(ageLabel);
+        ImageIcon ageFieldBg = new ImageIcon("src/resources/images/teste.png");
+        JLabel ageFieldBackground = new JLabel(ageFieldBg);
+        ageFieldBackground.setBounds(65, 300, 266, 47);
+        layeredPane.add(ageFieldBackground, JLayeredPane.DEFAULT_LAYER);
 
-        ageField = new JTextField();
-        ageField.setBackground(new Color(255, 255, 255));
-        ageField.setForeground(new Color(0, 0, 0));
-        ageField.setBorder(null);
-        ageField.setBounds(30, 235, 340, 30);
-        backgroundPanel.add(ageField);
-
+        ageField = transparentField(68, 300, 262, 47, 12);
+        layeredPane.add(ageField, JLayeredPane.PALETTE_LAYER);
 
         CustomButton submitButton = new CustomButton(
-                "src/resources/buttons/addButton.png",
-                120,
-                300,
+                "src/resources/buttons/submitButton.png",
+                116,
+                370,
                 165,
                 62,
                 e -> parentFrame.setState(JFrame.ICONIFIED),
                 Cursor.getPredefinedCursor(Cursor.HAND_CURSOR)
         );
 
-        backgroundPanel.add(submitButton);
+        layeredPane.add(submitButton, JLayeredPane.PALETTE_LAYER);
 
         add(backgroundPanel);
         setVisible(true);
+    }
+
+    private JTextField transparentField(int x, int y, int width, int height, int fontSize) {
+        JTextField field = new JTextField();
+        field.setOpaque(false);
+        Color fontColor = new Color(218, 195, 167);
+        field.setForeground(fontColor);
+        field.setBorder(null);
+        field.setBounds(x, y, width, height);
+        field.setFont(new Font("Arial", Font.BOLD, fontSize));
+        return field;
+    }
+
+    private void configFieldWithLabel(JLayeredPane panel, String labelText, int x, int y, int width, int height, int fontSize) {
+        JLabel label = new JLabel(labelText);
+        Color fontColor = new Color(218, 195, 167);
+        label.setForeground(fontColor);
+        label.setFont(new Font("Arial", Font.BOLD, fontSize));
+        label.setBounds(x, y, width, height);
+        panel.add(label, JLayeredPane.PALETTE_LAYER);
     }
 }

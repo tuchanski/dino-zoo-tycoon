89a634856340fb0f4baa43c2b48c6301bdfc4091
Tuchanski <gui.rocha2004@gmail.com>
2024-11-07 10:46:03 -0300

    :arrow_up: update: Database Diagram & Script.sql

--- database/script.sql
+++ database/script.sql
@@ -10,60 +10,53 @@ CREATE TABLE SystemUser (
 CREATE TABLE Zoo (
                      zoo_id BIGSERIAL PRIMARY KEY,
                      name VARCHAR(100) NOT NULL,
-                     location VARCHAR(100) NOT NULL,
                      user_id BIGINT REFERENCES SystemUser(user_id)
 );
 
 CREATE TABLE ParkEvent (
                            event_id BIGSERIAL PRIMARY KEY,
-                           name VARCHAR(100) NOT NULL,
-                           date DATE NOT NULL,
-                           zoo_id BIGINT REFERENCES Zoo(zoo_id)
-);
-
-CREATE TABLE Inventory (
-                           inventory_id BIGSERIAL PRIMARY KEY,
-                           zoo_id BIGINT REFERENCES Zoo(zoo_id)
+                           name VARCHAR(100) NOT NULL
 );
 
 CREATE TABLE Food (
                       food_id BIGSERIAL PRIMARY KEY,
                       name VARCHAR(50) NOT NULL,
-                      quantity INT NOT NULL,
-                      inventory_id BIGINT REFERENCES Inventory(inventory_id)
+                      type VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE Enclosure (
-                           enclosure_id BIGSERIAL PRIMARY KEY,
-                           name VARCHAR(50) NOT NULL,
-                           size INT NOT NULL,
-                           inventory_id BIGINT REFERENCES Inventory(inventory_id)
+CREATE TABLE MeatStock (
+                           stock_id BIGSERIAL PRIMARY KEY,
+                           quantity INT NOT NULL,
+                           zoo_id BIGINT REFERENCES Zoo(zoo_id) NOT NULL
+);
+
+CREATE TABLE EggStock (
+                          stock_id BIGSERIAL PRIMARY KEY,
+                          quantity INT NOT NULL,
+                          zoo_id BIGINT REFERENCES Zoo(zoo_id) NOT NULL
+);
+
+CREATE TABLE PlantStock (
+                            stock_id BIGSERIAL PRIMARY KEY,
+                            quantity INT NOT NULL,
+                            zoo_id BIGINT REFERENCES Zoo(zoo_id) NOT NULL
 );
 
 CREATE TABLE Dinosaur (
                           dinosaur_id BIGSERIAL PRIMARY KEY,
                           species VARCHAR(50) NOT NULL,
                           diet_type VARCHAR(50) NOT NULL,
-                          enclosure_id BIGINT REFERENCES Enclosure(enclosure_id)
+                          zoo_id BIGINT REFERENCES Zoo(zoo_id) NOT NULL
 );
 
 CREATE TABLE Visitor (
                          visitor_id BIGSERIAL PRIMARY KEY,
                          name VARCHAR(100) NOT NULL,
-                         visit_date DATE NOT NULL,
-                         inventory_id BIGINT REFERENCES Inventory(inventory_id)
+                         zoo_id BIGINT REFERENCES Zoo(zoo_id) NOT NULL
 );
 
 CREATE TABLE Employee (
                           employee_id BIGSERIAL PRIMARY KEY,
                           name VARCHAR(100) NOT NULL,
-                          job_title VARCHAR(100) NOT NULL,
-                          salary DECIMAL(10, 2) NOT NULL,
-                          inventory_id BIGINT REFERENCES Inventory(inventory_id)
-);
-
-CREATE TABLE Ticket (
-                        ticket_id BIGSERIAL PRIMARY KEY,
-                        price DECIMAL(5, 2) NOT NULL,
-                        visitor_id BIGINT REFERENCES Visitor(visitor_id)
+                          zoo_id BIGINT REFERENCES Zoo(zoo_id) NOT NULL
 );

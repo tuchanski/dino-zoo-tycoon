8a515576abb92b0659628b0590e33656c608bfd4
Tuchanski <gui.rocha2004@gmail.com>
2024-11-08 11:38:50 -0300

    :sparkles: feat: Add Employee Controller

---
+++ src/controllers/EmployeeController.java
@@ -0,0 +1,36 @@
+package controllers;
+
+import exceptions.EntityNotFoundException;
+import models.Employee;
+import models.Zoo;
+import repositories.EmployeeRepositoryImpl;
+import repositories.interfaces.IEmployeeRepository;
+
+import java.util.List;
+
+public class EmployeeController {
+
+
+    private final IEmployeeRepository employeeRepository;
+
+    public EmployeeController(Zoo zoo) {
+        employeeRepository = new EmployeeRepositoryImpl(zoo);
+    }
+
+    public void createEmployee() {
+        employeeRepository.createGenericEmployee();
+    }
+
+    public List<Employee> getEmployees() {
+        return employeeRepository.getEmployees();
+    }
+
+    public Employee getEmployeeById(int id) {
+        return employeeRepository.getEmployeeById((long) id);
+    }
+
+    public Employee deleteEmployeeById(int id) throws EntityNotFoundException {
+        return employeeRepository.deleteEmployeeById((long) id);
+    }
+    
+}

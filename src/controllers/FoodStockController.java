594327e35a87e6b2710580fdca1387e8f4a44a31
Tuchanski <gui.rocha2004@gmail.com>
2024-11-08 19:23:59 -0300

    :sparkles: feat: Add FoodStock Controller

---
+++ src/controllers/FoodStockController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import exceptions.EntityNotFoundException;
+import models.FoodStock;
+import models.Zoo;
+import repositories.FoodStockRepositoryImpl;
+import repositories.interfaces.IFoodStockRepository;
+
+import java.util.List;
+
+public class FoodStockController {
+
+    private final IFoodStockRepository stockRepository;
+
+    public FoodStockController(Zoo zoo) {
+       stockRepository = new FoodStockRepositoryImpl(zoo);
+    }
+
+    public void addFood(int foodId, int amount) throws EntityNotFoundException {
+        stockRepository.addFood((long) foodId, amount);
+    }
+
+    public void removeFood(int foodId, int amount) throws EntityNotFoundException {
+        stockRepository.removeFood((long) foodId, amount);
+    }
+
+    public List<FoodStock> getFoodStock() throws EntityNotFoundException {
+        return stockRepository.getFoodStock();
+    }
+
+}

f77c619f233d99bf83a3711dd29a1aca581b4aeb
Tuchanski <gui.rocha2004@gmail.com>
2024-11-09 11:19:02 -0300

    :arrow_up: update: Restructure Dinosaur Controller

--- src/controllers/DinosaurController.java
+++ src/controllers/DinosaurController.java
@@ -7,6 +7,7 @@ import models.Zoo;
 import repositories.DinosaurRepositoryImpl;
 import repositories.interfaces.IDinosaurRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DinosaurController {
@@ -17,29 +18,59 @@ public class DinosaurController {
         dinosaurRepository = new DinosaurRepositoryImpl(zoo);
     }
 
-    public void createDinosaur(String species) throws EntitySpeciesNotFound {
-        dinosaurRepository.createDinosaur(species);
+    public void createDinosaur(String species) {
+        try {
+            dinosaurRepository.createDinosaur(species);
+        } catch (EntitySpeciesNotFound e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
     public List<Dinosaur> getDinosaurs(){
         return dinosaurRepository.getDinosaurs();
     }
 
-    public Dinosaur getDinosaurById(int id) throws EntityNotFoundException {
+    public Dinosaur getDinosaurById(int id) {
         return dinosaurRepository.getDinosaurById(id);
     }
 
-    public List<Dinosaur> getDinosaursBySpecies(String species) throws EntitySpeciesNotFound {
-        return dinosaurRepository.getDinosaursBySpecies(species);
+    public List<Dinosaur> getDinosaursBySpecies(String species) {
+
+        List<Dinosaur> dinosaurs = new ArrayList<>();
+
+        try {
+            dinosaurs = dinosaurRepository.getDinosaursBySpecies(species);
+        } catch (EntitySpeciesNotFound e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return dinosaurs;
     }
 
-    public Dinosaur deleteDinosaurById(int id) throws EntityNotFoundException {
-        return dinosaurRepository.deleteDinosaurById(id);
+    public Dinosaur deleteDinosaurById(int id) {
+
+        Dinosaur dinosaur = null;
+
+        try {
+            dinosaur = dinosaurRepository.deleteDinosaurById(id);
+        } catch (EntityNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return dinosaur;
     }
 
-    public Dinosaur updateDinosaurById(int id, String newSpecies) throws EntityNotFoundException {
-        return dinosaurRepository.updateDinosaurById(id, newSpecies);
-    }
+    public Dinosaur updateDinosaurById(int id, String newSpecies) {
 
+        Dinosaur dinosaur = null;
+
+        try {
+            dinosaur = dinosaurRepository.updateDinosaurById(id, newSpecies);
+        } catch (EntityNotFoundException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        return dinosaur;
+    }
 
 }

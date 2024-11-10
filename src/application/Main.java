2eb1280f86829102c26395b5315bdbe453076806
Tuchanski <gui.rocha2004@gmail.com>
2024-11-09 22:51:52 -0300

    :arrow_up: update: Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,9 @@
 package application;
 
-import controllers.UserController;
-import controllers.ZooController;
-import exceptions.EntityAlreadyRegisteredException;
-import exceptions.EntityNotFoundException;
-import models.User;
-import models.Zoo;
-import models.enums.DinosaurSpecies;
-import models.enums.FoodType;
-import repositories.DinosaurRepositoryImpl;
-import repositories.FoodRepository;
-import repositories.FoodStockRepositoryImpl;
-import repositories.VisitorRepositoryImpl;
+import views.panels.InitialPanel;
 
 public class Main {
-    public static void main(String[] args) throws EntityAlreadyRegisteredException, EntityNotFoundException {
-
-
-
+    public static void main(String[] args) {
+        InitialPanel dinoZooTycoon = new InitialPanel();
     }
 }
